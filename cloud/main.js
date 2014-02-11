3240408cf1cd7e582f6da1478508d28776116226
Philip Hayes <phayes@Philips-MacBook-Pro-3.local>
2014-02-11 16:26:17 +0000

    script to populate

--- cloud/main.js
+++ cloud/main.js
@@ -1,5 +1,8 @@
 var util = require('util');
-var sync = require('./sync-srv.js');
+var sync = require('./sync-srv.js'),
+async = require('async'),
+csv = require('csv');
+_ = require('underscore');
 /* main.js
  * All calls here are publicly exposed as REST API endpoints.
  * - all parameters must be passed in a single JSON paramater.
@@ -30,4 +33,64 @@ sync.init(dataset_id, {}, function() {
   sync.removeCollision(dataset_id, dataHandler.removeCollision);
 });
 
+exports.createDummyData = function(params, callback){
 
+
+
+}
+
+
+exports.populateItems = function (params, callback) {
+
+    var writeFuncs = [];
+
+    $fh.db({
+        'act': 'deleteall',
+        'type': 'myDataSet'
+    }, function(err) {
+        if (err) {
+            console.log( err);
+        } else {
+            console.log('Successfully deleted items');
+
+            var entries = [];
+
+            csv().from.path(__dirname + '/data/items.csv', {
+                delimiter: ','
+            })
+                .to.array(function(data) {
+                    entries = data;
+                })
+                .on('end', function(count) {
+                    console.log('Number of items: ' + count + ' ' + entries.length);
+                    _.each(entries, function(item) {
+                    	console.log(item);
+                        writeFuncs.push(createWrite(item));
+                    });
+                    // We have to execute the creates in series because of a bug in fh mongo (race condiction create collection)
+                    async.series(writeFuncs);
+                });
+        }
+    });
+    callback(null,'{"status":"success"}');
+
+    function createWrite(item) {
+        // return closure with specific usrn
+        return function(callback) {
+            $fh.db({
+                act: 'create',
+                type: 'myDataSet',
+                fields: {
+                    name: item[0],
+                   
+                }
+            }, function(err) {
+                if (err) {
+                    console.log( err);
+                }
+                callback(null);
+            });
+        };
+    }
+
+};
