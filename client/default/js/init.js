601e288425e9de113b53592ca147ef0237d9607f
Philip Hayes <phayes@Philips-MacBook-Pro-3.local>
2014-02-11 10:54:19 +0000

    Initial changes

--- client/default/js/init.js
+++ client/default/js/init.js
@@ -18,6 +18,7 @@ $fh.ready(function() {
         act:'getConfig'
       },
       function(res) {
+        alert(res);
         document.getElementById('cloudConfig').innerHTML = "<p>" + JSON.stringify(res.config) + "</p>";
       },
       function(code,errorprops,params) {
--- cloud/main.js
+++ cloud/main.js
@@ -10,6 +10,7 @@ var util = require('util');
  */
 exports.getConfig = function(params, callback) {
   console.log("In getConfig() call");
+  console.log('test comment');
   var cfg = require("config.js");
   return callback(null, {config: cfg.config});
 };
